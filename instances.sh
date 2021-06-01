5b46c0835e9119689cc22b0251776b28c0a6e244
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-01 08:33:30 +0530

    rs-ansible

--- instances.sh
+++ instances.sh
@@ -4,14 +4,14 @@ if [ -z "$2" ]; then
 
   case $1 in
     launch)
-      for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
+      for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
         echo "Launching $component Spot Instance"
         aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0c67cf26ada14e11b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>>/tmp/instatances-launch
       done
     ;;
     routes)
       echo Updating routes
-      for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
+      for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
         IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
         sed -e "s/COMPONENT/${component}/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/${component}.json
         aws route53 change-resource-record-sets --hosted-zone-id Z0776868T2OXFHMTQL3I --change-batch file:///tmp/${component}.json
