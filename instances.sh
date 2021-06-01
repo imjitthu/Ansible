07ee1cd9bca3cc848e0741453c8f2f88d5b2bbe4
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-01 08:24:16 +0530

    rs-ansible

--- instances.sh
+++ instances.sh
@@ -6,7 +6,7 @@ if [ -z "$2" ]; then
     launch)
       for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
         echo "Launching $component Spot Instance"
-        aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0ab50da5bc0120a59 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>>/tmp/instatances-launch
+        aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0c67cf26ada14e11b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>>/tmp/instatances-launch
       done
     ;;
     routes)
@@ -14,7 +14,7 @@ if [ -z "$2" ]; then
       for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
         IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
         sed -e "s/COMPONENT/${component}/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/${component}.json
-        aws route53 change-resource-record-sets --hosted-zone-id Z077254017HKF6MBGS2JG --change-batch file:///tmp/${component}.json
+        aws route53 change-resource-record-sets --hosted-zone-id Z0776868T2OXFHMTQL3I --change-batch file:///tmp/${component}.json
       done
     ;;
   esac
@@ -24,7 +24,7 @@ else
     launch)
       for component in $2; do
         echo "Launching $component Spot Instance"
-        aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0ab50da5bc0120a59 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>>/tmp/instatances-launch
+        aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0c67cf26ada14e11b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>>/tmp/instatances-launch
       done
     ;;
     routes)
@@ -32,7 +32,7 @@ else
       for component in $2; do
         IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
         sed -e "s/COMPONENT/${component}/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/${component}.json
-        aws route53 change-resource-record-sets --hosted-zone-id Z077254017HKF6MBGS2JG --change-batch file:///tmp/${component}.json
+        aws route53 change-resource-record-sets --hosted-zone-id Z0776868T2OXFHMTQL3I --change-batch file:///tmp/${component}.json
       done
     ;;
   esac
