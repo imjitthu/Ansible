a7f7cfbc47e31d6fa94614fdfe94c18ff0c7a02e
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-14 21:45:41 +0530

    roboshop

--- mkinv.sh
+++ mkinv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
->/tmp/inv
+>inv
 for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress' |xargs)
   if [ -n "${IP}" ]; then
