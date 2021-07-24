d00f8c063b0bcfef7ae258a55fb3ca3e2aa6f44f
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-07-24 11:32:51 +0530

    Az DevOps

--- mkinv.sh
+++ mkinv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-> inv
+> /tmp/inv
 for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress' |xargs)
   if [ -n "${IP}" ]; then
-    echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >> inv
+    echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >> /tmp/inv
   fi
 done
